3b7e7448633db96e88898ba3b04acda62c2ccfac
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-05 18:51:14 +0900

    4.4 completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    return (
+        blogs.length !== 0
+        ? blogs.map(blog => blog.likes).reduce((likes, total) => total + likes)
+        : 0
+    )
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
