e771eee8235b2d6b84103fdf88794a305dbd3c71
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-07 18:26:19 +0900

    4.21 completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -25,6 +25,14 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)  
+  if (!decodedToken.id) {    
+    return response.status(401).json({ error: 'token invalid' })  
+  }  
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() !== decodedToken.id.toString()) {
+    return response.status(401).json({ error: 'blog not owned by this user' })
+  }
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
