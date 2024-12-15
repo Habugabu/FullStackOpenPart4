fe43efe4ec9e1c8d320323bc187a8b53b3833acd
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-15 14:18:27 +0900

    test router fix #2

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,7 +2,7 @@ const testRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-router.post('/reset', async (request, response) => {
+testRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
   await User.deleteMany({})
 
